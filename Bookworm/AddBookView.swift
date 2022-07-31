3a239e360c59e265937e4b602e782c5bcdb90717
NT <usedforfunny@gmail.com>
2022-07-31 23:45:37 +0200

    Challenge completes. Final commit

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,79 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Alexandre Talatinian on 30/07/2022.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Unknown"
+    @State private var review = ""
+    
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id:\.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    RatingView(rating: $rating)
+                } header: {
+                    Text("Write a review")
+                }
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: moc)
+                        
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+                        newBook.date = Date.now
+                        
+                        try? moc.save()
+                        dismiss()
+                    }
+                    .disabled(validation)
+                }
+            }
+            .navigationBarTitle("Add Book")
+        }
+    }
+    
+    var validation: Bool {
+        if title.trimmingCharacters(in: .whitespaces).isEmpty || author.trimmingCharacters(in: .whitespaces).isEmpty || review.trimmingCharacters(in: .whitespaces).isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
